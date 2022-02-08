e3ed3bb790d58a7708bd255185c27f543b6f0b1a
navI-asphxxk <maksim.nametchenko@gmail.com>
2022-02-08 22:17:16 +0300

    memory usage of vectorVoid

--- libs/data_structures/vector/vectorVoid.c
+++ libs/data_structures/vector/vectorVoid.c
@@ -1,1 +1,40 @@
 #include "vectorVoid.h"
+#include <stdio.h>
+
+vectorVoid createVectorV(size_t n, size_t baseTypeSize) {
+    void *data = (void *) malloc(baseTypeSize * n);
+    if (data == NULL) {
+        fprintf(stderr, "bad alloc ");
+        exit(1);
+    } else
+        return (vectorVoid) {data, 0, n, baseTypeSize};
+}
+
+void reserveV(vectorVoid *v, size_t newCapacity) {
+    v->capacity = newCapacity;
+    v->data = (void *) realloc(v->data, v->baseTypeSize * newCapacity);
+    if (v->data == NULL) {
+        fprintf(stderr, "bad alloc ");
+        exit(1);
+    } else if (newCapacity == 0) {
+        v->data = NULL;
+        v->size = 0;
+    } else if (newCapacity < v->size)
+        v->size = newCapacity;
+}
+
+void shrinkToFitV(vectorVoid *v) {
+    reserveV(v, v->size);
+}
+
+void clearV(vectorVoid *v) {
+    v->size = 0;
+}
+
+void deleteVectorV(vectorVoid *v) {
+    if (v->data == NULL) {
+        free(v->data);
+        v->data = NULL;
+    }
+}
+
--- libs/data_structures/vector/vectorVoid.h
+++ libs/data_structures/vector/vectorVoid.h
@@ -3,6 +3,10 @@
 
 #include <limits.h>
 #include <malloc.h>
+#include <stdint.h>
+#include <memory.h>
+#include <assert.h>
+#include <stdbool.h>
 
 typedef struct vectorVoid {
     void *data;
@@ -11,5 +15,30 @@ typedef struct vectorVoid {
     size_t baseTypeSize;
 } vectorVoid;
 
+// возвращает структуру-дескриптор вектор из n значений,
+// элементы которой имеют тип baseTypeSize
+// Если ОС не смогла выделить необходимый фрагмент памяти, то
+// вывод соответствующего сообщения
+vectorVoid createVectorV(size_t n, size_t baseTypeSize);
+
+// изменяет количество памяти, выделенное под хранение
+// newCapacity элементов вектора v
+// Если ОС не смогла выделить необходимый фрагмент памяти,
+// вывод соответствующего сообщения
+// Если в качестве newCapacity указано значение 0,
+// в v->data записывает NULL
+// Если значение newCapacity стало меньше v->size,
+// тогда значение v->size равняется newCapacity.
+void reserveV(vectorVoid *v, size_t newCapacity);
+
+// освобождает память, выделенную под
+// неиспользуемые элементы вектора v
+void shrinkToFitV(vectorVoid *v);
+
+// удаляет элементы вектора v, без освобождения память
+void clearV(vectorVoid *v);
+
+// освобождает память, выделенную вектору v
+void deleteVectorV(vectorVoid *v);
 
 #endif
