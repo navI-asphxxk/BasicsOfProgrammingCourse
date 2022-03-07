f259eaab609d32ce35db4ca8e2bf1cfc6c990460
navI-asphxxk <maksim.nametchenko@gmail.com>
2022-03-07 21:51:18 +0300

    task 6

--- libs/data_structures/string/tasks/isWordsOrderedLexically.h
+++ libs/data_structures/string/tasks/isWordsOrderedLexically.h
@@ -4,6 +4,22 @@
 #include "../string_.h"
 
 bool isWordsOrderedLexically (char *s) {
+    char *beginSearch = s;
+    WordDescriptor currentWord;
+    if (!getWord(beginSearch, &currentWord))
+        return true;
+
+    WordDescriptor previousWord = currentWord;
+    while (getWord(beginSearch, &currentWord)) {
+        if (areWordsEqual(currentWord, previousWord) < 0)
+            return false;
+
+        beginSearch = currentWord.end;
+        previousWord = currentWord;
+    }
+
+    return true;
+}
 
 }
 
